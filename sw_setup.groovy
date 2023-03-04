70e7eb82de62f78e4b33aaf96b19ba52dfc9e873
yoshikapriyatham <126958117+yoshikapriyatham@users.noreply.github.com>
2023-03-04 18:03:53 +0530

    Create sw_setup.groovy

---
+++ sw_setup.groovy
@@ -0,0 +1,27 @@
+pipeline{
+    agent{
+        label 'slave1'
+    }
+    stages{
+        stage('clean workspace'){
+            steps{
+              cleanWs()  
+            }
+            
+        }
+        stage('git checkout'){
+            steps{
+             git branch: 'main', credentialsId: 'anusha_git_creds', url: 'ssh://git@bitbucket.org:company/repo.git'
+            }
+            
+        }
+        stage('Build'){
+            steps{
+              script{
+               sh " ansible-playbook sw_install.yml"
+            }  
+            }
+            
+        }
+    }
+}
