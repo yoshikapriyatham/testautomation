6933d84c780a7f998f31048eb85e190ec308625a
yoshikapriyatham <126958117+yoshikapriyatham@users.noreply.github.com>
2023-03-05 17:24:12 +0530

    Update sw_setup.groovy

--- sw_setup.groovy
+++ sw_setup.groovy
@@ -17,7 +17,7 @@ pipeline{
         }
         stage('Build'){
             steps{
-                withCredentials([sshUserPrivateKey(credentialsId: 'node_creds', keyFileVariable: 'private_key', usernameVariable: 'user_name')]) {
+               /* withCredentials([sshUserPrivateKey(credentialsId: 'node_creds', keyFileVariable: 'private_key', usernameVariable: 'user_name')]) {
                     script{
                         //sh " ansible-playbook sw_install.yml disableHostKeyChecking: true"
                           sh """
@@ -25,16 +25,16 @@ pipeline{
                           ansible-playbook credentialsId: 'node_creds' sw_install.yml 
                           """
                         } 
-                        }
+                        }*/
                     
-                   /* script {
+                    script {
                     ansiblePlaybook(
                         playbook: './sw_install.yml',
                         credentialsId: 'node_creds',
                         disableHostKeyChecking: true,
                         colorized: true)
                 
-                    }*/
+                    }
             
             }
             
